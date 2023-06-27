311ad0190d8611f93eae83a10b7367d881a76ce9
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-27 10:44:13 -0400

    created an object to test the function

---
+++ script.js
@@ -0,0 +1,16 @@
+//Object Problem 3
+
+/*
+Create an object with three items in it, then write a JavaScript function 
+that accepts an object and tells you the length 
+(tells you how many items are in it) of the object.
+*/
+
+const aquemini = {
+	
+	artist: 'Outkast',
+	year: 1998,
+	label: 'LaFace',
+}
+
+console.log(Object.keys(aquemini));
